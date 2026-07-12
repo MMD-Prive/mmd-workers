082969c2dc6ad8448b3d6f2582dcc5bce35c6ad4
MMD Privé <malemodel.bkk@gmail.com>
2026-07-13 06:52:17 +0700

    Remove duplicate copied files from Kenji branch

--- admin-worker/src/lib/auth 2.ts
+++
@@ -1,6 +0,0 @@
-import type { Env } from "../types";
-
-export function isAuthorized(request: Request, env: Env): boolean {
-  const auth = request.headers.get("authorization");
-  return auth === `Bearer ${env.INTERNAL_TOKEN}`;
-}
