374acbeda6718cb68199ac39c975771c7674c545
MMD Privé <malemodel.bkk@gmail.com>
2026-08-28 16:09:06 +0700

    feat(member-pages): add resolver diagnostic RPC

--- member-pages-worker/src/runtime-index.js
+++ member-pages-worker/src/runtime-index.js
@@ -2,5 +2,6 @@ import worker from "./index.js";
 
 export * from "./legacy-member-pages.js";
 export { CareBackBirthdayWishCoordinator } from "./care-back-birthday-wish-durable-object.js";
+export { MemberResolverDiagnosticEntrypoint } from "./resolver-diagnostic-entrypoint.js";
 
 export default worker;
