d9a600c9b75503f3771581a480e5cd8754d82e8f
MMD Privé <malemodel.bkk@gmail.com>
2026-09-03 17:01:24 +0700

    fix(mms): port member prebooking read facade onto current main

--- member-pages-worker/src/index.js
+++ member-pages-worker/src/index.js
@@ -2,6 +2,7 @@ import liffFoundation from "./liff-identity-foundation.js";
 import { handleLiffMemberShell, isLiffMemberShellPath } from "./liff-member-shell.js";
 import { handlePublicCareBackWishRoute, isPublicCareBackWishPath } from "./public-care-back-wish.js";
 import { handleFindMemberApi, isFindMemberApiPath } from "./find-member-api.js";
+import { handleMmsMemberPrebookingRead, isMmsMemberPrebookingReadPath } from "./mms-member-prebooking-read.js";
 import {
   decorateLiffShellWithClientDiagnostic,
   handleLiffClientDiagnostic,
@@ -14,6 +15,7 @@ export { CareBackBirthdayWishCoordinator } from "./care-back-birthday-wish-coord
 export default {
   async fetch(request, env = {}, ctx) {
     const url = new URL(request.url);
+    if (request.method === "GET" && isMmsMemberPrebookingReadPath(url)) return handleMmsMemberPrebookingRead(request, env);
     if (isFindMemberApiPath(url)) return handleFindMemberApi(request, env);
     if (isLiffClientDiagnosticPath(url)) return handleLiffClientDiagnostic(request, env);
     if (isPublicCareBackWishPath(url)) return handlePublicCareBackWishRoute(request, env);
