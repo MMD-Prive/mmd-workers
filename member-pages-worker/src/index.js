90027375554c89e45807bd0f6c9dc6bf759f724c
MMD Privé <malemodel.bkk@gmail.com>
2026-09-04 11:42:21 +0700

    feat(member): route My MMD app API through member-pages worker

--- member-pages-worker/src/index.js
+++ member-pages-worker/src/index.js
@@ -3,6 +3,7 @@ import { handleLiffMemberShell, isLiffMemberShellPath } from "./liff-member-shel
 import { handlePublicCareBackWishRoute, isPublicCareBackWishPath } from "./public-care-back-wish.js";
 import { handleFindMemberApi, isFindMemberApiPath } from "./find-member-api.js";
 import { handleMmsMemberPrebookingRead, isMmsMemberPrebookingReadPath } from "./mms-member-prebooking-read.js";
+import { handleMemberAppApi, isMemberAppApiPath } from "./member-app-api.js";
 import {
   decorateLiffShellWithClientDiagnostic,
   handleLiffClientDiagnostic,
@@ -16,6 +17,7 @@ export default {
   async fetch(request, env = {}, ctx) {
     const url = new URL(request.url);
     if (request.method === "GET" && isMmsMemberPrebookingReadPath(url)) return handleMmsMemberPrebookingRead(request, env);
+    if (isMemberAppApiPath(url)) return handleMemberAppApi(request, env);
     if (isFindMemberApiPath(url)) return handleFindMemberApi(request, env);
     if (isLiffClientDiagnosticPath(url)) return handleLiffClientDiagnostic(request, env);
     if (isPublicCareBackWishPath(url)) return handlePublicCareBackWishRoute(request, env);
