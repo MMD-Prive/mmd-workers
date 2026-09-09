7394cbef7b4eb6ff7247dfac16bfa39d0d0ff01f
MMD Privé <malemodel.bkk@gmail.com>
2026-09-09 13:02:15 +0700

    feat(mms): expose dispatch durable object entrypoint

---
+++ mms-worker/src/runtime-index-with-dispatch.js
@@ -0,0 +1,5 @@
+import runtime from "./runtime-index-with-application-v4.js";
+export { MmsCoordinator } from "./runtime-index-with-application-v4.js";
+export { MmsDispatchCoordinator } from "./my-mms-dispatch-runtime.mjs";
+
+export default runtime;
