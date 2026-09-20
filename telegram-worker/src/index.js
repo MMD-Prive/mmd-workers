14b56a55226d9eaab4fe182a673104a3a8d230c8
MMD Privé <malemodel.bkk@gmail.com>
2026-09-20 11:08:25 +0700

    feat(telegram): accept dedicated payments service auth for internal alerts

--- telegram-worker/src/index.js
+++ telegram-worker/src/index.js
@@ -63,6 +63,7 @@ export default {
                 "AUTH_SERVICE_STUDIO_TO_TELEGRAM",
                 "AUTH_SERVICE_AUTH_TO_TELEGRAM",
                 "AUTH_SERVICE_LINE_TO_TELEGRAM",
+                "AUTH_SERVICE_PAYMENTS_TO_TELEGRAM",
               ]
             : [],
         });
