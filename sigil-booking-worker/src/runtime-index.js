8aa895493a22a4685c1547c5c03b54b4bd2addf6
MMD Privé <malemodel.bkk@gmail.com>
2026-09-21 13:25:51 +0700

    feat(profiles): route public R2 model catalog

--- sigil-booking-worker/src/runtime-index.js
+++ sigil-booking-worker/src/runtime-index.js
@@ -1,6 +1,7 @@
 import entitlementRuntime from "./entitlement-runtime-worker.js";
 import modelImagePolicyWorker from "./model-image-policy-worker.js";
 import { attachCareBackApprovalToConfirmedBooking } from "./care-back-trusted-caller.js";
+import { handlePublicProfilesCatalogRequest, isPublicProfilesCatalogRequest } from "./public-profiles-catalog.js";
 
 const AIRTABLE_API = "https://api.airtable.com/v0";
 const CLIENT_RESOLVE_PATH = "/sigil/api/client/resolve";
@@ -13,6 +14,10 @@ export default {
     const url = new URL(request.url);
     const path = normalizePath(url.pathname);
     const method = request.method.toUpperCase();
+
+    if (isPublicProfilesCatalogRequest(path, method)) {
+      return handlePublicProfilesCatalogRequest(request, env);
+    }
 
     if (method === "POST" && path === BOOKING_CONFIRM_PATH) {
       const approvalRequest = request.clone();
